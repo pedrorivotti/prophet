47ee34f0751c802ea140c09e95592560e81357ad
Bastien Vallet <b.vallet@criteo.com>
2021-06-04 15:59:39 +0200

    Test with a compatible version pystan<=2.18

--- python/setup.py
+++ python/setup.py
@@ -122,14 +122,14 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='prophet',
-    version='1.0.1+criteo1',
+    version='1.0.1+criteo2',
     description='Automatic Forecasting Procedure',
     url='https://facebook.github.io/prophet/',
     author='Sean J. Taylor <sjtz@pm.me>, Ben Letham <bletham@fb.com>',
     author_email='sjtz@pm.me',
     license='MIT',
     packages=find_packages(),
-    setup_requires=['pystan~=2.19.1.1'],
+    setup_requires=['pystan<=2.18'],
     install_requires=install_requires,
     python_requires='>=3',
     zip_safe=False,
